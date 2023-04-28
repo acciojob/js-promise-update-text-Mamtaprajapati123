2960f394602a2491616e017e4530d0cc6d5b87ee
Mamtaprajapati123 <124058185+Mamtaprajapati123@users.noreply.github.com>
2023-04-28 19:30:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-//your JS code here. If required.
+// Get the output element by ID
+const outputElement = document.getElementById("output");
+
+// Define a function that returns a promise
+function sayHello() {
+  return new Promise((resolve, reject) => {
+    // Use setTimeout to simulate an async operation that takes 1 second
+    setTimeout(() => {
+      resolve("Hello, world!"); // Resolve the promise with the string "Hello, world!"
+    }, 1000);
+  });
+}
+
+// Call the sayHello function and update the output element with the result
+sayHello().then((result) => {
+  outputElement.innerText = result;
+}).catch((error) => {
+  console.error(error);
+});
